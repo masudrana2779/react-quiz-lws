db1e03c198c9b626a5049cba2d7ec95148006fb2
Masud Rana <masudrana2779@gmail.com>
2025-05-21 10:24:56 +0600

    l-11 done but scroll bar problm

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,25 +1,66 @@
+import { useState } from "react";
+
+import InfiniteScroll from "react-infinite-scroll-component";
 import { Link } from "react-router-dom";
-import classes from "../styles/Videos.module.css";
 import Video from "./Video";
+import useVideosList from "../hooks/useVideosList";
 
 export default function Videos() {
+  const [page, setPage] = useState(1);
+  const { loading, error, videos, hasMore } = useVideosList(page);
+  console.log("🚀 ~ Videos ~ videos:", videos);
+  if (loading) {
+    return (
+      <div>
+        <h2>Loading...</h2>
+      </div>
+    );
+  }
   return (
-    <div className={classes.videos}>
-      <Link to="/quiz">
-        <Video />
-      </Link>
-      <Link to="/quiz">
-        <Video />
-      </Link>
-      <Link to="/quiz">
-        <Video />
-      </Link>
-      <Link to="/quiz">
-        <Video />
-      </Link>
-      <Link to="/quiz">
-        <Video />
-      </Link>
+    <div>
+      {videos.length > 0 && (
+        <InfiniteScroll
+          dataLength={videos.length}
+          hasMore={hasMore}
+          next={() => {
+            setPage(page + 8);
+          }}
+          loader={
+            <div>
+              <h2>Loading...</h2>
+            </div>
+          }
+        >
+          {videos.map((video) =>
+            video.noq > 0 ? (
+              <Link key={video.youtubeID} to="/quiz">
+                <Video
+                  title={video.title}
+                  id={video.youtubeID}
+                  noq={video.noq}
+                />
+              </Link>
+            ) : (
+              <Video
+                key={video.youtubeID}
+                title={video.title}
+                id={video.youtubeID}
+                noq={video.noq}
+              />
+            )
+          )}
+        </InfiniteScroll>
+      )}
+      {!loading && videos.length === 0 && (
+        <div>
+          <h2>No Videos Available</h2>
+        </div>
+      )}
+      {error && (
+        <div>
+          <h2>There was an error!!</h2>{" "}
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/hooks/useVideosList.js
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import {
+  getDatabase,
+  orderByKey,
+  query,
+  ref,
+  get,
+  startAt,
+  limitToFirst,
+} from "firebase/database";
+
+export default function useVideosList(page) {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [videos, setVideos] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
+
+  useEffect(() => {
+    async function fetchVideos() {
+      // database related works
+      const db = getDatabase();
+      const videosRef = ref(db, "videos");
+      const videoQuery = query(
+        videosRef,
+        orderByKey(),
+        startAt("" + page),
+        limitToFirst(8)
+      );
+
+      try {
+        setError(false);
+        setLoading(true);
+        // get the data from firebase
+        const snapshot = await get(videoQuery);
+        setLoading(false);
+        if (snapshot.exists()) {
+          setVideos((prevVideos) => {
+            return [...prevVideos, ...Object.values(snapshot.val())];
+          });
+        } else {
+          setHasMore(false);
+        }
+      } catch (err) {
+        console.log(err);
+        setLoading(false);
+        setError(true);
+      }
+    }
+
+    setTimeout(() => {
+      fetchVideos();
+    }, 2000);
+  }, [page]);
+  return { loading, error, videos, hasMore };
+}
